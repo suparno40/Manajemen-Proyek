3d931325ad0f922e59897861b9be905275ef1822
supar <omsupra89@gmail.com>
2026-05-21 15:20:00 +0700

    Add SpeedInsights component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
-import { Analytics } from '@vercel/analytics/react'; // 1. TAMBAHKAN BARIS INI DI PALING ATAS
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next'; // 1. TAMBAHKAN BARIS INI
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-        <Analytics /> {/* 2. TAMBAHKAN BARIS INI DI SINI */}
+        <Analytics />
+        <SpeedInsights /> {/* 2. TAMBAHKAN BARIS INI */}
       </body>
     </html>
   );
